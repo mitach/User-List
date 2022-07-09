ba47619b9ed407ffa4fb9b28b035a6f6b72ada92
mitach <malinovd02@gmail.com>
2022-07-09 16:29:48 +0300

    feat: fetching and rendering users

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { Header } from './components/common/Header';
+import { Footer } from './components/common/Footer';
+import { Search } from './components/search/Search';
+import { UserList } from './components/user-list/UserList';
+
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+const baseUrl = 'http://localhost:3005/api';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch(`${baseUrl}/users`)
+            .then(res => res.json())
+            .then((data) => {
+                setUsers(data.users);
+            })
+    }, []);
+
+    return (
+        <div>
+            <Header />
+
+            <main className="main">
+                <section className="card users-container">
+                    <Search />
+
+                    <UserList users={users} />
+                </section>
+            </main>
+
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/common/Footer.js
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>All Copyrights Reserved © 2022, June</p>
+        </footer>
+    );
+}
