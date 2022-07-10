cfc418e3359ed39004a166acf4390d6d0338ecb5
mitach <malinovd02@gmail.com>
2022-07-10 09:32:04 +0300

    feat: Add user service

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,7 @@
+import { useEffect, useState } from 'react';
+
+import * as userService from './services/userService';
+
 import { Header } from './components/common/Header';
 import { Footer } from './components/common/Footer';
 import { Search } from './components/search/Search';
@@ -5,18 +9,13 @@ import { UserList } from './components/user-list/UserList';
 
 import './App.css';
 
-import { useEffect, useState } from 'react';
-
-const baseUrl = 'http://localhost:3005/api';
 
 function App() {
     const [users, setUsers] = useState([]);
+
     useEffect(() => {
-        fetch(`${baseUrl}/users`)
-            .then(res => res.json())
-            .then((data) => {
-                setUsers(data.users);
-            })
+        userService.getAll()
+            .then(users => setUsers(users));
     }, []);
 
     return (
